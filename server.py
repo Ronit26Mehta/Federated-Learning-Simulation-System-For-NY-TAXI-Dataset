4964b44736965fc57d20abd807bab123cb8a1a2e
Ronit Mehta <109467924+Ronit26Mehta@users.noreply.github.com>
2024-10-16 23:00:54 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ strategy = fl.server.strategy.FedAvg()
 if __name__ == "__main__":
     # Get the port from the environment variable, defaulting to 8080 if not set
     port = os.getenv("PORT", 8080)
-    server_address = f"0.0.0.0:{port}"
+    server_address = f"35.164.95.156:{port}"
     print(f"Starting global server at {server_address}...")
 
     # Start the Flower server on the specified port
