97782fcd4171a484376250c56ec86e8ab53e707b
Ronit Mehta <109467924+Ronit26Mehta@users.noreply.github.com>
2024-10-16 23:18:24 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ strategy = fl.server.strategy.FedAvg()
 
 if __name__ == "__main__":
     # Get the port from the environment variable, defaulting to 8080 if not set
-    port = os.getenv("PORT", 8080)
+    port = os.getenv("PORT", 0)
     server_address = f"35.164.95.156:{port}"
     print(f"Starting global server at {server_address}...")
 
