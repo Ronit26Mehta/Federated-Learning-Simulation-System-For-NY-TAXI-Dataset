c50257139df92d3aa9a6761433dcab72ddb254c9
Ronit Mehta <109467924+Ronit26Mehta@users.noreply.github.com>
2024-10-16 22:21:29 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,4 +6,4 @@ strategy = fl.server.strategy.FedAvg()
 # Start the server with the specified strategy
 if __name__ == "__main__":
     print("Starting global server...")
-    fl.server.start_server(server_address="127.0.0.1:8080", strategy=strategy)
+    fl.server.start_server(server_address="0.0.0.0:8080", strategy=strategy)
